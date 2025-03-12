a0719a473d9f9530a21dc1907bf7639932f761b2
yuproper <yuproper@gmail.com>
2025-03-13 05:51:33 +0700

    initial
    initial

---
+++ PS05_divanpars/PS05_divanpars/spiders/divannewpars.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["divan.ru"]
+    start_urls = ["https://www.divan.ru/tomsk/category/svet"]
+
+    def parse(self, response):
+        pass
